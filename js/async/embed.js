b4c2867c44253216d0b3e3517a1e5270338b9814
Mike Auclair <mike@mikeauclair.com>
2015-06-19 16:52:23 -0400

    slightly better encapsulation

--- js/async/embed.js
+++ js/async/embed.js
@@ -1,10 +1,7 @@
 window.EmbedPOC.bindBehavior = function(){
-  var lastFocus;
   var dispatchedMessage;
-
-  var modalContainer = document.createElement('div');
-  var messageContainer = document.createElement('div');
-
+  var body = document.body;
+  
   var preloadResource = function(path){
     var xhr = new XMLHttpRequest();
     xhr.open('GET', path);
@@ -13,37 +10,42 @@ window.EmbedPOC.bindBehavior = function(){
 
   var clickHandler = function(attribute, modal) {
     return function(event){
-      if (event.target.getAttribute(attribute)) {
-        var modalData = event.target.getAttribute(attribute);
+      var target = event.target,
+          modalData = target.getAttribute(attribute);
+      if (modalData) {
+        
         var iFrameSrc = "embed.html?input="+modalData;
         var iFrame = '<iframe aria-live="polite" src="'+iFrameSrc+'"></iframe>';
-        lastFocus = event.target || document.activeElement;
-        modal.$.modalContent.setAttribute('data-content-to-load', iFrame);
+        modal.lastFocus = target;
+        modal.fill(iFrame);
         dispatchedMessage = modalData;
-        modal.open('#' +modalContainer.id);
+        modal.open();
+        
       }
     }
   };
 
-  var messageHandler = function(event){
-    if (event.origin != window.location.origin) return;
-    try {
-      var payload = JSON.parse(event.data);
-    } catch(err) {
-      return;
-    }
+  var messageHandler = function(output){
+    return function(event){
+      if (event.origin != window.location.origin) return;
+      try {
+        var payload = JSON.parse(event.data);
+      } catch(err) {
+        return;
+      }
 
-    var replyWith = function(message){
-      event.source.postMessage(JSON.stringify({reply: message}), window.location.origin);
-    }
-    
-    if (payload.init){
-      messageContainer.innerHTML = "<p>The iFrame initialized with:</p>"+payload.init;
-      replyWith("Hello back from the parent. In case you forgot, the value I sent you initially was: " + dispatchedMessage);
-    } else if (payload.message){
-      messageContainer.innerHTML = "<p>The iFrame sent:</p>"+payload.message;
-      replyWith("Thanks for sending " + payload.message);
-    }
+      var replyWith = function(message){
+        event.source.postMessage(JSON.stringify({reply: message}), window.location.origin);
+      }
+      
+      if (payload.init){
+        output.innerHTML = "<p>The iFrame initialized with:</p>"+payload.init;
+        replyWith("Hello back from the parent. In case you forgot, the value I sent you initially was: " + dispatchedMessage);
+      } else if (payload.message){
+        output.innerHTML = "<p>The iFrame sent:</p>"+payload.message;
+        replyWith("Thanks for sending " + payload.message);
+      }
+    };
   };
 
   var buildModal = function(){
@@ -53,14 +55,14 @@ window.EmbedPOC.bindBehavior = function(){
     modalBox.setAttribute('role','dialog');
     modalBox.setAttribute('aria-labelledby', 'iFrame');
     modalBox.innerHTML = '<div class="modal-inner"><a href="javascript:" rel="modal:close" aria-label="Close" class="close">&times;</a><div class="modal-content"></div></div>';
-    
+
+    var modalContainer = document.createElement('div');
     modalContainer.id = 'iFrameWidget';
     modalContainer.style.display = 'none';
-    document.body.appendChild(modalBox);
-    document.body.appendChild(modalContainer);
-    
-    
-    return new VanillaModal({
+    body.appendChild(modalBox);
+    body.appendChild(modalContainer);
+
+    var vm = new VanillaModal({
       onBeforeClose: function(){
         if (this.$.modalContent.innerHTML){
           // If you just set it to null, IE gives it a null
@@ -69,9 +71,9 @@ window.EmbedPOC.bindBehavior = function(){
         }
       },
       onClose: function(){
-        if (lastFocus){
-          lastFocus.focus();
-          lastFocus = null;
+        if (modal.lastFocus){
+          modal.lastFocus.focus();
+          modal.lastFocus = null;
         }
       },
       onOpen: function(){
@@ -81,13 +83,24 @@ window.EmbedPOC.bindBehavior = function(){
       },
       closeKey: 27
     });
+    
+    var modal = {
+      open: function(){
+        vm.open('#'+ modalContainer.id);
+      },
+      fill: function(content){
+        vm.$.modalContent.setAttribute('data-content-to-load', content);
+      }
+    };
+    return modal;
   };
 
   var modal = buildModal();
-  document.querySelector('body').addEventListener('click', clickHandler('data-modal', modal));
+  body.addEventListener('click', clickHandler('data-modal', modal));
 
-  document.body.appendChild(messageContainer);
-  window.addEventListener("message", messageHandler, false);
+  var messageContainer = document.createElement('div');
+  body.appendChild(messageContainer);
+  window.addEventListener("message", messageHandler(messageContainer), false);
   
   preloadResource('build/iframe.js');
   preloadResource('css/iframe.css');
