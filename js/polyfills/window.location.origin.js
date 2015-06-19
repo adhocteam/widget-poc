6385d681016f98f23b32c23d96b30b4513407914
Mike Auclair <mike@mikeauclair.com>
2015-06-19 10:50:39 -0400

    Thanks IE

---
+++ js/polyfills/window.location.origin.js
@@ -0,0 +1,4 @@
+// IE polyfill
+if (!window.location.origin) {
+  window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');
+}
