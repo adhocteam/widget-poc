f69a1e983e77d53e39668ed5f0173e9ddfbed715
Mike Auclair <mike@mikeauclair.com>
2015-07-07 20:45:05 -0400

    Add navigation command tests

--- test/modal_test.js
+++ test/modal_test.js
@@ -15,7 +15,7 @@ var server = http.createServer(function(req, res) {
 server.listen(8357);
 
 var url = "http://localhost:8357"
-
+var iFrame = '.modal-content iframe';
 describe('opening a modal', function(){
   this.timeout(30000);
 
@@ -23,28 +23,55 @@ describe('opening a modal', function(){
     new Nightmare()
       .goto(url)
       .click('.overlay.all')
-      .wait('.modal-content')
-      .evaluate(function(){
-        return document.querySelector('.modal-content')
-          .getElementsByTagName('iframe')[0]
-          .contentDocument
+      .wait('.modal-visible')
+      .evaluate(function(iFrame){
+        return document.querySelector(iFrame).contentDocument
           .getElementsByTagName('p')[0]
           .innerText
       }, function(text){
         expect(text).to.equal('Content goes here')
-      }).run(done);
+      }, iFrame).run(done);
   });
 
   it('should display coverage information', function(done){
     new Nightmare()
       .goto(url)
       .wait('.planDetails')
-      .screenshot('screen.jpg')
       .evaluate(function(){
         return document.querySelectorAll('.planDetails')[1]
           .getElementsByTagName('li')[0].innerText
       }, function(text){
         expect(text).to.equal('Doctors: 6 of 6')
-      }).run(done)
-  })
+      })
+      .click('[data-plan-id="123456"] div.planDetails a')
+      .wait('.modal-visible')
+      .evaluate(function(iFrame){
+        return document.querySelector(iFrame).contentDocument
+          .getElementsByTagName('div')[0].innerText
+      }, function(text){
+        expect(text).to.include('"planID":"123456"')
+      }, iFrame)
+      .run(done);
+  });
+
+  it('should display overlay rollup and provide navigation instructions on click', function(done){
+    new Nightmare()
+      .goto(url)
+      .wait('.overlayDetails')
+      .evaluate(function(){
+        return document.querySelector('.overlayDetails')
+          .getElementsByTagName('li')[0].innerText
+      }, function(text){
+        expect(text).to.include('Doctors: 6')
+      })
+      .click('.overlayDetails li a')
+      .wait('.modal-visible')
+      .evaluate(function(iFrame){
+        return document.querySelector(iFrame).contentDocument
+          .getElementsByTagName('div')[0].innerText
+      }, function(text){
+        expect(text).to.include('"section":"doctors"')
+      }, iFrame)
+      .run(done)
+  });
 });
