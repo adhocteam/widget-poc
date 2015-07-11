0aa34eb862d21a3763f6d487b36e2bd585e2867f
Mike Auclair <mike@mikeauclair.com>
2015-07-11 11:31:12 -0400

    more removal coverage

--- test/modal_test.js
+++ test/modal_test.js
@@ -45,13 +45,33 @@ describe('opening a modal', function(){
     it('should remove a facilitity', function(done){
       horseman.click('.facilities-list li a')
       expect(horseman.text('facilities-page')).to.include('No facilities added')
-      horseman.click('a[rel="modal:close"]')
-      horseman.page.switchToParentFrame()
-      var text = horseman.evaluate(function(){
-        return $('.planDetails')[1].getElementsByTagName('li')[2].innerText
-      })
-      expect(text).to.include('0 of 0')
+      
+      
       done()                               
+    })
+
+    describe('having removed a facility', function(){
+      beforeEach(function(){
+        horseman.click('.facilities-list li a')
+        horseman.click('a[rel="modal:close"]')
+        horseman.page.switchToParentFrame()
+      })
+
+      it('should no longer be in the rollup', function(done){
+        var text = horseman.evaluate(function(){
+          return $('.planDetails')[1].getElementsByTagName('li')[2].innerText
+        })
+        expect(text).to.include('0 of 0')
+        done()
+      })
+
+      it('should not have a facility block in the plan details', function(done){
+        horseman.click('[data-plan-id="123456"] div.planDetails a')
+        horseman.waitForSelector('.modal-visible')
+          .switchToChildFrame(0)
+        expect(horseman.count('h4')).to.equal(2)
+        done()
+      })
     })
     
   })
