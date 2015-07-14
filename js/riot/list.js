37822a08bb274fb77799b51ec5c070ad30d3538b
Mike Auclair <mike@mikeauclair.com>
2015-07-14 10:26:26 -0400

    build

--- js/riot/list.js
+++ js/riot/list.js
@@ -1,3 +1,6 @@
-riot.tag('list', '<doctors-page></doctors-page><scrips-page></scrips-page><facilities-page></facilities-page>', function(opts) {
-
+riot.tag('list', '<doctors-page></doctors-page><scrips-page></scrips-page><facilities-page></facilities-page><div class="nav"><a href="javascript:;" onclick="{navToSearch}">Add</a></div>', function(opts) {
+    navToSearch = function(){
+      riot.route('search');
+    }
+  
 });
---
+++ js/riot/search-panel.js
@@ -0,0 +1,4 @@
+riot.tag('search-panel', '<form onsubmit="{search}"><label> Search <input name="query"><button>Search</button></label></form><div class="results"><div class="doctors" if="{results.doctors.length}"><h4>Doctors</h4><ul><li each="{results.doctors}"> {name} {specialty} <a href="javascript:" onclick="{parent.addDoctor}">Add</a></li></ul></div><div class="drugs" if="{results.drugs.length}"><h4>Drugs</h4><ul><li each="{results.drugs}"> {name} <a href="javascript:" onclick="{parent.addDrug}">Add</a></li></ul></div><div class="facilities" if="{results.facilities.length}"><h4>Facilities</h4><ul><li each="{results.facilities}"> {name} <a href="javascript:" onclick="{parent.addFacility}">Add</a></li></ul></div></div>', function(opts) {
+    WidgetApp.controllers['search'].init(this);
+  
+});
