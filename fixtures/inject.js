e01c7c67dd62562c1c072f3ffbcfe41d2ca1652f
Mike Auclair <mike@mikeauclair.com>
2015-07-18 13:38:59 -0400

    new fixtures

---
+++ fixtures/inject.js
@@ -0,0 +1,5 @@
+(function(){
+  var entities = WidgetApp.store.entities;
+  entities.providerUrl = 'http://localhost:8357/fixtures/providers.json';
+  entities.drugUrl = 'http://localhost:8357/fixtures/drugs.json';
+})();
