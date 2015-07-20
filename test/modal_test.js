c32a2ea91fd5ccae94fb93aca59102fb82cf2a78
Mike Auclair <mike@mikeauclair.com>
2015-07-20 10:18:44 -0400

    test updates + semicolon audit

--- test/modal_test.js
+++ test/modal_test.js
@@ -9,23 +9,23 @@ describe('opening a modal', function(){
   it('should display coverage information and navigate to plan on click', function(done){
     horseman.waitForSelector('.planDetails');
     var text = horseman.evaluate(function(){
-      return $('.planDetails')[1].getElementsByTagName('li')[0].innerText
+      return $('.planDetails')[1].getElementsByTagName('li')[0].innerText;
     })
-    expect(text).to.equal('Doctors: 4 of 6')
-    horseman.click('[data-plan-id="123456"] div.planDetails a')
+    expect(text).to.equal('Doctors: 4 of 6');
+    horseman.click('[data-plan-id="123456"] div.planDetails a');
     horseman.waitForSelector('.modal-visible')
-      .switchToChildFrame(0)
-    expect(horseman.text('h3')).to.include('Plan ID: 123456')
-    expect(horseman.count('coverage-set li')).to.equal(13)
+      .switchToChildFrame(0);
+    expect(horseman.text('h3')).to.include('Plan ID: 123456');
+    expect(horseman.count('coverage-set li')).to.equal(13);
     done();
   });
 
   it('should display overlay rollup', function(done){
     horseman.waitForSelector('.overlayDetails')
     var text = horseman.evaluate(function(){
-      return $('.overlayDetails li')[0].innerText
+      return $('.overlayDetails li')[0].innerText;
     })
-    expect(text).to.include('Doctors: 6')
+    expect(text).to.include('Doctors: 6');
     done();
   });
   
@@ -34,40 +34,40 @@ describe('opening a modal', function(){
       horseman.waitForSelector('.overlayDetails')
         .click('.overlayDetails a')
         .waitForSelector('.modal-visible')
-        .switchToChildFrame(0)
+        .switchToChildFrame(0);
     })
     
     it('should show doctors', function(done){
-      expect(horseman.text('h3')).to.include('Doctors')
+      expect(horseman.text('h3')).to.include('Doctors');
       done();
     })
 
     it('should remove a facilitity', function(done){
-      horseman.click('.facilities-list li a')
-      expect(horseman.text('facilities-page')).to.include('No facilities added')
+      horseman.click('.facilities-list li a');
+      expect(horseman.text('facilities-page')).to.include('No facilities added');
       done();
     })
 
     describe('having removed a facility', function(){
       beforeEach(function(){
-        horseman.click('.facilities-list li a')
-        horseman.click('a[rel="modal:close"]')
-        horseman.page.switchToParentFrame()
+        horseman.click('.facilities-list li a');
+        horseman.click('a[rel="modal:close"]');
+        horseman.page.switchToParentFrame();
       })
 
       it('should no longer be in the rollup', function(done){
         var text = horseman.evaluate(function(){
-          return $('.planDetails')[1].getElementsByTagName('li')[2].innerText
+          return $('.planDetails')[1].getElementsByTagName('li')[2].innerText;
         })
-        expect(text).to.include('0 of 0')
+        expect(text).to.include('0 of 0');
         done();
       })
 
       it('should not have a facility block in the plan details', function(done){
         horseman.click('[data-plan-id="123456"] div.planDetails a')
         horseman.waitForSelector('.modal-visible')
-          .switchToChildFrame(0)
-        expect(horseman.count('h4')).to.equal(2)
+          .switchToChildFrame(0);
+        expect(horseman.count('h4')).to.equal(2);
         done();
       })
     })
@@ -82,8 +82,8 @@ describe('opening a modal', function(){
       it('should search and find a drug', function(done){
         horseman.click(".nav a")
           .type('input[name="query"]', 'flutic')
-          .click("button").waitForSelector('search-panel').screenshot('screen.png')
-        expect(horseman.count('.drugs li')).to.equal(1)
+          .click("button").waitForSelector('search-panel');
+        expect(horseman.count('.drugs li')).to.equal(2);
         done();
       });
 
@@ -91,7 +91,7 @@ describe('opening a modal', function(){
         beforeEach(function(){
           horseman.click(".nav a")
             .type('input[name="query"]', 'flutic')
-            .click("button").waitForSelector('search-panel')
+            .click("button").waitForSelector('search-panel');
         });
 
         it('should add a drug', function(done){
@@ -99,9 +99,9 @@ describe('opening a modal', function(){
             .click('a[rel="modal:close"]');
           horseman.page.switchToParentFrame();
           var text = horseman.evaluate(function(){
-            return $('.planDetails')[1].getElementsByTagName('li')[1].innerText
-          })
-          expect(text).to.equal('Prescriptions: 3 of 4')
+            return $('.planDetails')[1].getElementsByTagName('li')[1].innerText;
+          });
+          expect(text).to.equal('Prescriptions: 3 of 4');
           done();
         });
         
